ee2bacce1e670b60594b482eb379a6943e43a068
Michel-Camacho <Michel_Sebastian@outlook.com>
2024-10-29 13:28:21 -0300

    se agrago carrusel de imagenes

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ const cerrarFormulario = document.querySelector("cerrarFormulario");
 const form = document.getElementsByTagName("form");
 const inicioDeSesion = document.querySelector(".inicio-de-sesion")
 
+const menuButton = document.querySelector(".menuButton");
+const sideBar = document.querySelector(".sidebar");
+const closeMenu = document.querySelector(".closeMenu");
+const menuLinks = document.querySelectorAll(".sidebar a[href^='#']");
+const scrollBtn = document.getElementById("scrollBtn");
+
 inputs.forEach(input => {
     input.required = true
 })
@@ -18,3 +24,21 @@ inicioDeSesion.addEventListener('click', function () {
 });
 
 
+menuButton.addEventListener("click", (showSideBAR) => {
+    sideBar.style.display = "block";
+});
+
+closeMenu.addEventListener("click", (hideSideBar) => {
+    sideBar.style.display = "none";
+});
+
+menuLinks.forEach((menuLinks) => {
+    menuLinks.addEventListener("click", function () {
+        sideBar.style.display = "none";
+    });
+});
+
+scrollBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
